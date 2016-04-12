4d03472fb8bcadfe2e1d797e0963f7abd1488f65
Boyquotes <webmaster@linux-live-cd.org>
2016-04-12 11:24:30 +0200

    Solution requete http

--- js/controller.js
+++ js/controller.js
@@ -1,29 +1,7 @@
 angular.module('alumniApp', [])
-.controller('MainCtrl', ['$scope', function ($scope) {
-  $scope.alumnis = [
-    {
-    "lastname": "Dude",
-    "firstname": "Abdallah",
-    "description": "Symfony, Magento",
-    "cvlink": "https://www.doyoubuzz.com/fr/p/modeles-cv/classic"
-    },
-    {
-    "lastname": "Dude",
-    "firstname": "Aladine",
-    "description": "NodeJS, React",
-    "cvlink": "https://www.doyoubuzz.com/fr/p/modeles-cv/classic"
-    },
-    {
-    "lastname": "Dude",
-    "firstname": "Alix",
-    "description": "SPIP, Wordpress",
-    "cvlink": "https://www.doyoubuzz.com/fr/p/modeles-cv/classic"
-    },
-    {
-    "lastname": "Dude",
-    "firstname": "André",
-    "description": "PHP7",
-    "cvlink": "https://www.doyoubuzz.com/fr/p/modeles-cv/classic"
-    },
-  ];
+.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+                $http.get("http://iness.simplon.co/alumnis.json").success(        function(data, status, headers) {
+                  console.log(data);
+                  $scope.alumnis = data;
+                });
 }]);
